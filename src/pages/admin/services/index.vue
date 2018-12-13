49336802f7782b36e1ab8c77ee93e3f2c0710572
Tamara Acevedo <tamara.acevedoavila@gmail.com>
2018-12-13 10:18:50 -0300

    detakke

--- src/pages/admin/services/index.vue
+++ src/pages/admin/services/index.vue
@@ -71,7 +71,7 @@
                         style="border-bottom: 1px solid #ccc;"
                       >
                         <v-list-tile-content>
-                          <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+                          <v-list-tile-title class="primary--text text-capitalize">{{ item.name }}</v-list-tile-title>
                           <v-list-tile-sub-title class="text--primary">{{ item.rut }}</v-list-tile-sub-title>
                           <!-- <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title> -->
                         </v-list-tile-content>
